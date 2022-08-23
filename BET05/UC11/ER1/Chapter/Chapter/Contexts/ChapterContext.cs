9d92df24199c6a020d8cde036200daae7f3cedcb
Luiz Carlos Barradas <100002313+LCBarradas@users.noreply.github.com>
2022-08-23 19:46:23 -0300

    Add files via upload

---
+++ BET05/UC11/ER1/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,30 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+
+        }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options): base(options)
+        {
+
+        }
+        //Vamos utilizar esse metodo para configurar o banco de dados
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                //cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-M021IKH\\SQLEXPRESS; initial catalog = Chapter Integrate Security = true");
+            }
+        }
+        //dbset representa as entidades que serão utilizadas nas operaçoes de leitura, criacao, atualizacao e delecao
+        public DbSet<Livro>Livros { get; set; }
+    }
+}
